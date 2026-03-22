8df1ad6af9ac55d23947fe72603aa7c3512036a8
majy2026 <372840680@qq.com>
2026-03-23 00:41:42 +0800

    refactor: 重构需求进展页面筛选条件状态管理
    将 selectedVersion 单一值改为 filterParams 对象，支持多条件筛选

--- web/apps/web-ele/src/views/feature-analysis/progress/index.vue
+++ web/apps/web-ele/src/views/feature-analysis/progress/index.vue
@@ -3,26 +3,26 @@ import { ref } from 'vue';
 
 import { Page } from '@vben/common-ui';
 
-import FilterBar from './components/FilterBar.vue';
+import FilterBar, { type ProgressFilterParams } from './components/FilterBar.vue';
 import PieCharts from './components/PieCharts.vue';
 import FeatureTable from './components/FeatureTable.vue';
 
 defineOptions({ name: 'FeatureProgress' });
 
-const selectedVersion = ref<string | undefined>();
+const filterParams = ref<ProgressFilterParams>({});
 </script>
 
 <template>
   <Page auto-content-height>
     <div class="feature-progress p-4">
       <!-- 筛选区域 -->
-      <FilterBar v-model="selectedVersion" />
+      <FilterBar v-model="filterParams" />
 
       <!-- 饼图区域 -->
-      <PieCharts :version="selectedVersion" />
+      <PieCharts :version="filterParams.version" />
 
       <!-- 数据表格区域 -->
-      <FeatureTable :version="selectedVersion" />
+      <FeatureTable :filter-params="filterParams" />
     </div>
   </Page>
 </template>
